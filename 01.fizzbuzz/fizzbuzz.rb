c498815bc5d3804544ee597fa236c819bb5b9832
akira.fujimura <akira.fujimura@luxiar.com>
2025-05-15 13:35:25 +0900

    修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
-(1..20).each do |i|
-  if (i % 3).zero? && (i % 5).zero?
-    puts 'FizzBuzz'
-  elsif (i % 3).zero?
-    puts 'Fizz'
-  elsif (i % 5).zero?
-    puts 'Buzz'
-  else
-    puts i
-  end
+for i in 1..20
+    if i % 3 == 0 && i % 5 == 0
+        puts "FizzBuzz"
+    elsif i % 3 == 0
+        puts "Fizz"
+    elsif i % 5 == 0
+        puts "Buzz"
+    else
+        puts i
+    end
 end
