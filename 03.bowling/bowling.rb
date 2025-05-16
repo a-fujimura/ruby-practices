3d349f591faadd0f286a85c3c57bd2d7cbafb887
akira.fujimura <akira.fujimura@luxiar.com>
2025-05-16 15:56:11 +0900

    rubocop実行

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,44 +1,40 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
 
-require 'debug' 
+require 'debug'
 
-score = ARGV[0].split(",")
+score = ARGV[0].split(',')
 
-score.map! { |n| n == "X" ? Integer(10) : Integer(n)  }
+score.map! { |n| n == 'X' ? Integer(10) : Integer(n) }
 
 frame_cnt = 1
 nageta_cnt = 0
 nageta_total_cnt = 0
 score_total = 0
 
-for i in score
-    score_total += i
-    if frame_cnt < 10
-        # ストライク
-        if nageta_cnt == 0 && i > 9
-            score_total += score[nageta_total_cnt + 1]
-            score_total += score[nageta_total_cnt + 2]
-            nageta_cnt = 0
-            frame_cnt += 1
-        # 1投目
-        elsif nageta_cnt == 0
-            nageta_cnt += 1
-        # 2投目
-        else 
-            # スペア
-            if score[nageta_total_cnt - 1] + i > 9
-                score_total += score[nageta_total_cnt + 1]
-            end
+score.each do |i|
+  score_total += i
+  next unless frame_cnt < 10
 
-            nageta_cnt = 0
-            frame_cnt += 1
-        end
+  # ストライク
+  if nageta_cnt.zero? && i > 9
+    score_total += score[nageta_total_cnt + 1]
+    score_total += score[nageta_total_cnt + 2]
+    nageta_cnt = 0
+    frame_cnt += 1
+  # 1投目
+  elsif nageta_cnt.zero?
+    nageta_cnt += 1
+  # 2投目
+  else
+    # スペア
+    score_total += score[nageta_total_cnt + 1] if score[nageta_total_cnt - 1] + i > 9
 
-        nageta_total_cnt += 1
+    nageta_cnt = 0
+    frame_cnt += 1
+  end
 
-    else
-        # 10フレーム以降は足すだけ
-    end
+  nageta_total_cnt += 1
 end
 
 puts score_total
