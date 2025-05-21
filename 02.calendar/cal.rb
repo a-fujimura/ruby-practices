0668870fa7bb0d6d4507a21124224cba4f9deeaa
a-fujimura <akr9915@gmail.com>
2025-05-21 14:47:42 +0900

    PR指摘内容修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,45 +1,38 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require 'date'
-require 'optparse'
+require "date"
+require "optparse"
 
-def show_wday(day)
-    wday_list = ["月","火","水","木","金","土","日"]
-    return wday_list[day]
+def show_calendar(year, month)
+  date = Date.new(year, month, -1)
+  if date.month < 10
+    puts "     #{date.month}月 #{date.year}年"
+  else
+    puts "    #{date.month}月 #{date.year}年"
+  end
+  puts "日 月 火 水 木 金 土"
+  print "   " * Date.new(year, month, 1).wday
+
+  (1..date.day).each do |i|
+    if i < 10
+      puts " #{i} "
+    else
+      puts "#{i} "
+    end
+    puts "" if Date.new(year, month, i).saturday?
+  end
 end
 
-def show_calendar(year,month)
-    date = Date.new(year,month,-1)
-    if date.month < 10
-        puts "     #{date.month}月 #{date.year}年"
-    else
-        puts "    #{date.month}月 #{date.year}年"
-    end
-    puts "日 月 火 水 木 金 土"
-    print "   " * (Date.new(year,month,1).wday)
+today = Time.now
 
-    for i in 1..date.day
-        wday = Date.new(year,month,i).wday
-        if i < 10
-            print " #{i} "
-        else
-            print "#{i} "
-        end
-        if wday == 6
-            puts ""
-        end
-    end
-end
-
-today = DateTime.now
-
-if ARGV.length > 0
-    params = ARGV.getopts("y:","m:")
-    if(params["y"] == nil)
-        show_calendar(today.year,Integer(params["m"]))
-    else
-        show_calendar(Integer(params["y"]),Integer(params["m"]))
-    end
+if ARGV.length.positive?
+  params = ARGV.getopts("y:", "m:")
+  if params["y"].nil?
+    show_calendar(today.year, Integer(params["m"]))
+  else
+    show_calendar(Integer(params["y"]), Integer(params["m"]))
+  end
 else
-    show_calendar(today.year,today.month)
+  show_calendar(today.year, today.month)
 end
