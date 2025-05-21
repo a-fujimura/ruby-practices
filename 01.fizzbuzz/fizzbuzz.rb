8a9dc28d48a736cac7a1e8b2d19bd562c68c4c29
a-fujimura <akr9915@gmail.com>
2025-05-21 12:12:29 +0900

    eachも試す(個人的にはこっちの方が好み)

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-20.times do |i|
-  counter = i + 1
-  if (counter % 3).zero? && (counter % 5).zero?
+(1..20).each do |i|
+  if (i % 3).zero? && (i % 5).zero?
     puts 'FizzBuzz'
-  elsif (counter % 3).zero?
+  elsif (i % 3).zero?
     puts 'Fizz'
-  elsif (counter % 5).zero?
+  elsif (i % 5).zero?
     puts 'Buzz'
   else
-    puts counter
+    puts i
   end
 end
