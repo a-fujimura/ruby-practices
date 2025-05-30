215c7d1a72ffb2b29e131d4032c3080b29ad2e85
a-fujimura <akr9915@gmail.com>
2025-05-30 15:52:57 +0900

    rjust他修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,38 +5,29 @@ require "date"
 require "optparse"
 
 def show_calendar(year, month)
-  date = Date.new(year, month, -1)
-  if date.month < 10
-    puts "     #{date.month}月 #{date.year}年"
+  first_date = Date.new(year, month, 1)
+  last_date = Date.new(year, month, -1)
+  if first_date.month < 10
+    puts "     #{first_date.month}月 #{first_date.year}年"
   else
-    puts "    #{date.month}月 #{date.year}年"
+    puts "    #{first_date.month}月 #{first_date.year}年"
   end
   puts "日 月 火 水 木 金 土"
   print "   " * Date.new(year, month, 1).wday
 
-  (1..date.day).each do |i|
-    if i < 10
-      print " #{i} "
-    else
-      print "#{i} "
-    end
-    puts "" if Date.new(year, month, i).saturday?
+  (first_date..last_date).each do |date|
+    print "#{date.day.to_s.rjust(2)} "
+    puts "" if date.saturday?
   end
 
-  if !date.saturday?
+  unless last_date.saturday?
     puts "" # ターミナルの終端出力(%)を出力しないための改行
   end
 end
 
-today = Time.now
+today = Date.today
+params = ARGV.getopts("y:", "m:")
+year = params["y"].nil? ? today.year : params["y"].to_i
+month = params["m"].nil? ? today.month : params["m"].to_i
 
-if ARGV.length.positive?
-  params = ARGV.getopts("y:", "m:")
-  if params["y"].nil?
-    show_calendar(today.year, Integer(params["m"]))
-  else
-    show_calendar(Integer(params["y"]), Integer(params["m"]))
-  end
-else
-  show_calendar(today.year, today.month)
-end
+show_calendar(year, month)
