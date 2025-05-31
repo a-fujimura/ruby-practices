826dbc328ad7fa6c569e4f607eaa63907ff4f67a
a-fujimura <akr9915@gmail.com>
2025-06-01 01:31:10 +0900

    rjsut再対応/first_data再利用/後置unless/mainメゾット実装

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,30 +4,30 @@
 require "date"
 require "optparse"
 
+def main
+  today = Date.today
+  params = ARGV.getopts("y:", "m:")
+  year = params["y"].nil? ? today.year : params["y"].to_i
+  month = params["m"].nil? ? today.month : params["m"].to_i
+
+  show_calendar(year, month)
+end
+
 def show_calendar(year, month)
   first_date = Date.new(year, month, 1)
   last_date = Date.new(year, month, -1)
-  if first_date.month < 10
-    puts "     #{first_date.month}月 #{first_date.year}年"
-  else
-    puts "    #{first_date.month}月 #{first_date.year}年"
-  end
+
+  puts "#{first_date.month}月 #{first_date.year}年".rjust(14)
+
   puts "日 月 火 水 木 金 土"
-  print "   " * Date.new(year, month, 1).wday
+  print "   " * first_date.wday
 
   (first_date..last_date).each do |date|
     print "#{date.day.to_s.rjust(2)} "
     puts "" if date.saturday?
   end
 
-  unless last_date.saturday?
-    puts "" # ターミナルの終端出力(%)を出力しないための改行
-  end
+  puts "" unless last_date.saturday?
 end
 
-today = Date.today
-params = ARGV.getopts("y:", "m:")
-year = params["y"].nil? ? today.year : params["y"].to_i
-month = params["m"].nil? ? today.month : params["m"].to_i
-
-show_calendar(year, month)
+main
