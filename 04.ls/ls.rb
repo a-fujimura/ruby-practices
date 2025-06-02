aeb6ca5351cd79067df0975517419aa61070b7ea
a-fujimura <akr9915@gmail.com>
2025-06-02 12:22:05 +0900

    rubocop

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,7 +4,7 @@
 COL_COUNT = 3
 
 def main
-  files = Dir.glob("*")
+  files = Dir.glob('*')
   return unless files.length.positive?
 
   length_max = files.max_by(&:length).length
@@ -16,7 +16,7 @@ def convert_matrix(array, col_count)
   row_count = array.length.ceildiv(col_count)
 
   matrix = array.each_slice(row_count).map do |slice|
-    slice.fill("", slice.size...row_count)
+    slice.fill('', slice.size...row_count)
   end
   matrix.transpose
 end
