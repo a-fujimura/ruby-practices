21872e0a74e808305e5560c2bc09ba8c2ede85c3
a-fujimura <akr9915@gmail.com>
2025-06-02 12:21:32 +0900

    !を原則使わないに準拠

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,7 +4,7 @@
 COL_COUNT = 3
 
 def main
-  files = Dir.glob('*')
+  files = Dir.glob("*")
   return unless files.length.positive?
 
   length_max = files.max_by(&:length).length
@@ -16,14 +16,14 @@ def convert_matrix(array, col_count)
   row_count = array.length.ceildiv(col_count)
 
   matrix = array.each_slice(row_count).map do |slice|
-    slice.fill('', slice.size...row_count)
+    slice.fill("", slice.size...row_count)
   end
   matrix.transpose
 end
 
 def print_matrix(matrix, length_max)
   (0...matrix.length).each do |i|
-    matrix[i].map! { |element| element.ljust(length_max) }
+    matrix[i] = matrix[i].map { |element| element.ljust(length_max) }
     print matrix[i].join("\t")
     puts
   end
