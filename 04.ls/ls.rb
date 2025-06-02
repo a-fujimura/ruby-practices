b83f617e67777e7d8cce199b1e3535a811bd1a65
a-fujimura <akr9915@gmail.com>
2025-06-02 12:10:07 +0900

    length_maxの修正とrubocop

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,28 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
 COL_COUNT = 3
 
 def main
   files = Dir.glob("*")
-  if files.length > 0
-    filenames_matrix = convert_matrix(files, COL_COUNT)
-    print_matrix(filenames_matrix)
-  end
+  return unless files.length.positive?
+
+  length_max = files.max_by(&:length).length
+  filenames_matrix = convert_matrix(files, COL_COUNT)
+  print_matrix(filenames_matrix, length_max)
 end
 
 def convert_matrix(array, col_count)
-  length_max = array.max_by(&:length).length
   row_count = array.length.ceildiv(col_count)
 
   matrix = array.each_slice(row_count).map do |slice|
     slice.fill("", slice.size...row_count)
   end
-  return matrix.transpose
+  matrix.transpose
 end
 
-def print_matrix(matrix_data)
-  (0...matrix_data.length).each do |i|
-    print matrix_data[i].join("\t")
+def print_matrix(matrix, length_max)
+  (0...matrix.length).each do |i|
+    matrix[i].map! { |element| element.ljust(length_max) }
+    print matrix[i].join("\t")
     puts
   end
 end
