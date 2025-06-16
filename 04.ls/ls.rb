4164fa0d52b1ac55f4ff8e1353b9170d8727c8c6
a-fujimura <akr9915@gmail.com>
2025-06-16 16:55:49 +0900

    機能実装

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,21 +2,21 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'shellwords'
+require 'etc'
 
+PERMISSION_LIST = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'].freeze
 COL_COUNT = 3
 
 def main
   show_all = false
   show_reverse = false
+  show_longformat = false
 
   opt = OptionParser.new
-  opt.on('-a') do |option|
-    show_all = option
-  end
-
-  opt.on('-r') do |option|
-    show_reverse = option
-  end
+  opt.on('-a') { |option| show_all = option }
+  opt.on('-r') { |option| show_reverse = option }
+  opt.on('-l') { |option| show_longformat = option }
   opt.parse!
 
   flags = show_all ? File::FNM_DOTMATCH : 0
@@ -27,7 +27,12 @@ def main
 
   length_max = filenames_array.max_by(&:length).length
   filenames_matrix = convert_filenames_matrix(filenames_array, COL_COUNT)
-  print_filenames_matrix(filenames_matrix, length_max)
+
+  if show_longformat
+    print_filenames_array(filenames_array)
+  else
+    print_filenames_matrix(filenames_matrix, length_max)
+  end
 end
 
 def convert_filenames_matrix(filenames_array, col_count)
@@ -43,6 +48,33 @@ def print_filenames_matrix(filenames_matrix, length_max)
   filenames_matrix.each do |filenames|
     print filenames.map { |filename| filename.ljust(length_max) }.join("\t")
     puts
+  end
+end
+
+def print_filenames_array(filenames_array)
+  hardlink_max = filenames_array.map { |filename| File.stat(filename).nlink.to_s.length }.max
+  filesize_max = filenames_array.map { |filename| File.stat(filename).size.to_s.length }.max
+  filenames_array.each do |filename|
+    next unless filename.length.positive?
+
+    file_stat = File.stat(filename)
+    file_type = file_stat.ftype == 'file' ? '-' : 'd'
+    permission = file_stat.mode & 0o777
+    user_perm  = PERMISSION_LIST[(permission / 0o100) % 0o10]
+    group_perm = PERMISSION_LIST[(permission / 0o10)  % 0o10]
+    other_perm = PERMISSION_LIST[permission % 0o10]
+    xattr_option = `xattr -l #{Shellwords.escape(filename)}`.length.positive? ? '@' : ' '
+
+    filename_info = "#{file_type}#{user_perm}#{group_perm}#{other_perm}#{xattr_option} "
+    filename_info += "#{file_stat.nlink.to_s.rjust(hardlink_max)} "
+    filename_info += "#{Etc.getpwuid(file_stat.uid).name}  "
+    filename_info += "#{Etc.getgrgid(file_stat.gid).name}  "
+    filename_info += "#{file_stat.size.to_s.rjust(filesize_max)} "
+    filename_info += "#{file_stat.mtime.strftime('%_m')} "
+    filename_info += "#{file_stat.mtime.strftime('%_d')} "
+    filename_info += "#{file_stat.mtime.strftime('%H:%M')} "
+    filename_info += filename.to_s
+    puts filename_info
   end
 end
 
